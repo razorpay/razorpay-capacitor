6b8e005f4971e98c449ebb51e2f0b9849162e5ab
Sachin Nautiyal <sachinnautiyal00@gmail.com>
2021-01-21 23:36:38 +0530

    updated plugin sample

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Plugins } from '@capacitor/core';
+import { AlertController } from '@ionic/angular';
+
 import 'capacitor-razorpay';
 
 const { Checkout } = Plugins;
@@ -11,10 +13,20 @@ const { Checkout } = Plugins;
 })
 export class HomePage {
  
-  constructor() {
+  constructor(public alertController: AlertController) {
   }
  
-  async loadContacts() {
-     await Checkout.echo( { value: "https://ionicframework.com" });
+  async loadCheckout() {
+    const options = { value: 'string', key: 'rzp_test_1DP5mmOlF5G5ag', amount: '5000', description: 'Credits towards consultation', image: 'https://i.imgur.com/3g7nmJC.png', currency: 'INR', name: 'foo', prefill: { email: 'void@razorpay.com', contact: '9191919191', name: 'Razorpay Software'}, theme: {color: '#F37254'}}
+    let data = (await Checkout.open(options));
+    this.presentAlert(data.value);
+  }
+
+  async presentAlert( description: string) {
+    const alert = await this.alertController.create({
+        message: description,
+        buttons: ['Agree']
+    });
+    await alert.present();
   }
 }
