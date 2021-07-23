868bd3d458d0f920e3d4fcda870d244fa42afdfc
vivekshindhe <vivek.shindhe@razorpay.com>
2021-07-23 09:27:21 +0530

    Capacitor 3.0 web migration complete

--- dist/esm/definitions.js
+++ dist/esm/definitions.js
@@ -1,1 +1,6 @@
+// declare module '@capacitor/core' {
+//   interface PluginRegistry {
+//     Checkout: CheckoutPlugin;
+//   }
+// }
 //# sourceMappingURL=definitions.js.map
--- src/definitions.ts
+++ src/definitions.ts
@@ -1,8 +1,8 @@
-declare module '@capacitor/core' {
-  interface PluginRegistry {
-    Checkout: CheckoutPlugin;
-  }
-}
+// declare module '@capacitor/core' {
+//   interface PluginRegistry {
+//     Checkout: CheckoutPlugin;
+//   }
+// }
 
 export interface CheckoutPlugin {
   echo(options: { value: string }): Promise<{ value: string }>;
