c8abb0fd563b29cf780088a365c67f4261b5c7fe
vivekshindhe <vivek.shindhe@razorpay.com>
2022-03-31 16:27:21 +0530

    updated value of framework that is sent to the payload.

--- android/src/main/java/com/ionicframework/capacitor/Checkout.java
+++ android/src/main/java/com/ionicframework/capacitor/Checkout.java
@@ -34,7 +34,7 @@ public class Checkout extends Plugin  {
             JSObject jsObject = call.getData();
             Intent intent = new Intent(getActivity(), CheckoutActivity.class);
             intent.putExtra("OPTIONS", jsObject.toString());
-            intent.putExtra("FRAMEWORK", "CAPACITOR");
+            intent.putExtra("FRAMEWORK", "capacitor");
             startActivityForResult(call,intent,"handleOnActivityResult");
 
         } catch (Exception e) {
