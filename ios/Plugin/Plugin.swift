3f505c5dd032037b86fa74d73f19089aed4e07a2
vivekshindhe <vivek.shindhe@razorpay.com>
2022-05-06 02:22:53 +0530

    Updated error response to give all data

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -34,6 +34,15 @@ public class Checkout: CAPPlugin {
 }
 
 extension Checkout : RazorpayPaymentCompletionProtocolWithData, ExternalWalletSelectionProtocol {
+    
+    static let CODE_PAYMENT_ERROR = 1
+    
+    static let NETWORK_ERROR = 0
+        static let INVALID_OPTIONS = 1
+        static let PAYMENT_CANCELLED = 2
+        static let TLS_ERROR = 3
+        static let INCOMPATIBLE_PLUGIN = 3
+        static let UNKNOWN_ERROR = 100
     public func onPaymentSuccess(_ payment_id: String, andData response: [AnyHashable : Any]?) {
         if let call = call {
             var responseObj = [String:Any]()
@@ -42,9 +51,20 @@ extension Checkout : RazorpayPaymentCompletionProtocolWithData, ExternalWalletSe
             call.resolve(["response":(response) as Any])
         }
     }
-    public func onPaymentError(_ code: Int32, description str: String, andData response: [AnyHashable : Any]?) {
+    public func onPaymentError(_ code: Int32, description str: String, andData data: [AnyHashable : Any]?) {
         if let call = call {
-            call.reject("\(code): \(str)")
+            var response = [String: Any]()
+            do{
+                let dataJson = try JSONSerialization.data(withJSONObject: (data! as NSDictionary), options: [])
+                let dataString = String(data: dataJson, encoding: .utf8)!
+                call.reject("", dataString, nil)
+            }catch{
+                print("catch")
+            }
+            
+            
+            
+            
         }
     }
     
@@ -53,4 +73,17 @@ extension Checkout : RazorpayPaymentCompletionProtocolWithData, ExternalWalletSe
             call.reject(walletName)
         }
     }
+    
+    static func translateRzpPaymentError(errorCode: Int) -> Int {
+            switch (errorCode) {
+            case 0:
+                return NETWORK_ERROR
+            case 1:
+                return INVALID_OPTIONS
+            case 2:
+                return PAYMENT_CANCELLED
+            default:
+                return UNKNOWN_ERROR
+            }
+        }
 }
